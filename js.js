ca96e15711dbac537a5c3057feffdcc630f2eae4
Dafa Syachrullah <dafadafa2211@gmail.com>
2024-07-06 12:44:34 +0700

    first commit

---
+++ js.js
@@ -0,0 +1,8 @@
+console.log('js js')
+
+let add = (a, b) => a + b
+console.log(add(1, 2))
+
+let deduct = (a, b) => a - b
+
+
---
+++ try.py
@@ -0,0 +1,9 @@
+def print_kwargs(**kwargs):
+    for key, value in kwargs.items():
+        print(f"{key}: {value}")
+
+print_kwargs(name="John", age=30, city="New York")
+# Output:
+# name: John
+# age: 30
+# city: New York
